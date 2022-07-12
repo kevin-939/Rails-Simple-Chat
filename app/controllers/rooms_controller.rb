54aff8eccf8e3807cf09d5f461b7dd225ae8eac0
KC489 <kchrist773939@gmail.com>
2022-07-12 21:16:29 +1000

    room/show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -10,4 +10,15 @@ class RoomsController < ApplicationController
   def create
     @room = Room.create(name: params["room"]["name"])
   end
+
+  def show
+    @current_user = current_user
+    @single_room = Room.find(params[:id])
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room = Room.new
+  
+    render "index"
+  end
+  
 end
