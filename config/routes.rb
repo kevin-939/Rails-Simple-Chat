8ec14ecc8657c532dc1bb573846bb3bfe7e227dd
KC489 <kchrist773939@gmail.com>
2022-07-12 20:42:00 +1000

    room/controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,9 @@ Rails.application.routes.draw do
   get '/signin', to: 'sessions#new'
   post '/signin', to: 'sessions#create'
   delete '/signout', to: 'sessions#destroy'
+
+  get 'rooms/index'
+  resources :rooms
+  resources :users
+  root 'rooms#index'
 end
