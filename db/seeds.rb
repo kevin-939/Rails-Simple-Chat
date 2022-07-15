2a48d32d4c53001e27ea716b6d99a2e2c955cc75
KC489 <kchrist773939@gmail.com>
2022-07-15 16:48:35 +1000

    seed example

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Room.create(name: "Room 1")
+User.create(username: "User 1")
+User.create(username: "User 2")
