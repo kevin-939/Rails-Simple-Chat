a16a107cbecd4432092b4d5f214144494fefb367
KC489 <kchrist773939@gmail.com>
2022-07-18 19:21:32 +1000

    simple notification system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,20 @@
 class SessionsController < ApplicationController
-
   def create
     user = User.find_by(username: params[:session][:username])
     if user
       log_in(user)
     else
-      render 'new'
+      flash.notice = params[:session][:username]+" username has not been registered."
+      re_try
     end
+  end
+
+  def re_try
+    redirect_to root_path
   end
 
   def destroy
     log_out if logged_in?
     redirect_to root_path
   end
-
 end
